9cc530f0d4fb01668a6c5330bd4c8330b996dc0e
Swastika Yadav <Swastikayadav5358@gmail.com>
2023-09-23 23:33:40 +0530

    creating home page

--- weather-app/src/app/page.tsx
+++ weather-app/src/app/page.tsx
@@ -1,12 +1,22 @@
 import Image from 'next/image'
 
+import { Card, Divider, Subtitle, Text } from '@tremor/react';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#18387E] p-10 flex flex-col justify-center items-center" >
+    <Card className="max-w-4xl mx-auto bg-white rounded-md">  
+        <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
+        <Subtitle className="text-xl text-center">
+          Built with OpenAI, Nex.js, Tailwind CSS, Tremor, GraphQL and more!
+        </Subtitle>
 
+        <Divider className="my-10"/>
 
-
-      <h1>Weather App with ChatGPT</h1>
-    </main>
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#18387E]"> 
+        {/*CityPicker*/} 
+        </Card>
+      </Card>  
+    </div>
   )
 }
