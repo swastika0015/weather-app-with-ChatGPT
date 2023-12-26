accbdc0f289b27dd41dfee4ddaaca43159c24cdd
Swastika Yadav <Swastikayadav5358@gmail.com>
2023-12-26 21:43:50 +0530

    code imrove

--- weather-app/components/CityPicker.tsx
+++ weather-app/components/CityPicker.tsx
@@ -1,6 +1,6 @@
 import {Country, City} from "country-state-city";
 import Select from "react-select";
-import { useState } from "react";
+import { useState } from "react"; 
 import { useRouter } from 'next/navigation';
 import { GlobeIcon } from '@heroicons/react/solid';
 
