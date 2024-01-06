f0011dff037851de23fe7ae988058511e0172e7f
Swastika Yadav <Swastikayadav5358@gmail.com>
2024-01-07 00:35:03 +0530

    rain chart

--- weather-app/components/RainChart.tsx
+++ weather-app/components/RainChart.tsx
@@ -19,4 +19,34 @@ function RainChart({ results }: Props) {
     )
     .slice(currentHour, currentHour + 24);
 
+    const data = hourly.map((hour, i) => ({
+        time: Number(hour),
+        "Today": results.hourly.precipitation_probability[currentHour + i],
+        "Tomorrow": results.hourly.precipitation_probability[currentHour + i + 24],
+        "Next Day": results.hourly.precipitation_probability[currentHour + i + 48],
+      }));
+    
+      const dataFormatter = (number: number) => `${number} %`;
+    
+      return (
+        <div>
+          <Card>
+            <Title>Chances of Rain</Title>
+            <AreaChart
+              className="mt-6"
+              data={data}
+              showLegend
+              index="time"
+              categories={["Today", "Tomorrow", "Next Day"]}
+              colors={["indigo", "blue", "emerald"]}
+              minValue={0}
+              maxValue={100}
+              valueFormatter={dataFormatter}
+              yAxisWidth={50}
+            />
+          </Card>
+        </div>
+      );
+    }
+
 export default RainChart;
